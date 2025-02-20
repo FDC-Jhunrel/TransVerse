d285e5291737144f8eddf60ff994b85155bba24a
FDC-Jhunrel <fdc.jhunrel@gmail.com>
2025-02-20 16:14:28 +0800

    deep-source : issue

---
+++ app/src/main/java/com/example/speechtranslation/Activities/Checker/mPayment.kt
@@ -0,0 +1,10 @@
+package com.example.speechtranslation.Activities.Checker
+
+class mPayment {
+
+        fun failingMethod() {
+            val number: Int = "This is a string" // Type mismatch error
+            println(number)
+        }
+
+}
