720ccba66f551f88cc2c6becb4db1016c4fd469a
FDC-Jhunrel <fdc.jhunrel@gmail.com>
2025-02-20 16:20:48 +0800

    deep-source : issue 2

---
+++ app/src/main/java/com/example/speechtranslation/Activities/Checker/mTransactionHistory.kt
@@ -0,0 +1,9 @@
+package com.example.speechtranslation.Activities.Checker
+
+class mTransactionHistory {
+
+    fun viewTransact() {
+        val number : String = 1
+        println(number)
+    }
+}
