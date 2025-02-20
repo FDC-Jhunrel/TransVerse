c37a8ae65aee5124416f4d229490a3904fcafd6c
FDC-Jhunrel <fdc.jhunrel@gmail.com>
2025-02-20 15:16:20 +0800

    deep-source : code change 2 type mismatch

---
+++ app/src/main/java/com/example/speechtranslation/Activities/Checker/FailClass.kt
@@ -0,0 +1,8 @@
+package com.example.speechtranslation.Activities.Checker
+
+class FailClass {
+    fun failingMethod() {
+        val number: Int = "This is a string" // Type mismatch error
+        println(number)
+    }
+}
